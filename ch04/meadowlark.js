bdadc1edada8af3b11518dd81a572e82cc45771c
Floodeenjr <tmf@mgc.mentorg.com>
2017-08-01 16:01:17 -0600

    Changes to make these projects work

--- ch04/meadowlark.js
+++ ch04/meadowlark.js
@@ -4,7 +4,7 @@ var fortune = require('./lib/fortune.js');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express3-handlebars')
+var handlebars = require('express-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
