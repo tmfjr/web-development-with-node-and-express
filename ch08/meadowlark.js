7e39bcfd051bc75583f64cfc494a9276985012dc
Floodeenjr <tmf@mgc.mentorg.com>
2017-08-02 14:21:39 -0600

    Fixing errors and warnings

--- ch08/meadowlark.js
+++ ch08/meadowlark.js
@@ -102,8 +102,7 @@ app.get('/thank-you', function(req, res){
 	res.render('thank-you');
 });
 app.get('/newsletter', function(req, res){
-    // we will learn about CSRF later...for now, we just
-    // provide a dummy value
+    // we will learn about CSRF later...for now, we just provide a dummy value
     res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
 app.post('/process', function(req, res){
