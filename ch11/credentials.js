d2fe81a747044c5ac389b710781a9298e0376fd9
Thomas Floodeen, Jr <tmfjr@floodeen.com>
2017-08-07 13:16:29 -0600

    Remove file.

--- ch11/credentials.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-        cookieSecret: 'your cookie secret goes here',
-        gmail: {
-                user: 'bidsiemens',
-                password: '1/2baked',
-        }
-};
